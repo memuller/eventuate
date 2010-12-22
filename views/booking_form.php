5c4b69746b31afd3797e8d90ada16ea171b7382c
marcos-paiva <eu.marcos.paiva@gmail.com>
2010-12-21 18:39:19 -0800

    Alteração de nome de classe, alterando input por select para estados e adicionando alerta quanto a dados do cartão de crédito

--- views/booking_form.php
+++ views/booking_form.php
@@ -1,79 +1,115 @@
+<section id="alert" class="clearfix">
+   <img src="<?php bloginfo('template_directory'); ?>/course/images/cartao.jpg" alt="Aten√ß√£o nos pagamentos no cart√£o"/>
+   <p>
+      <strong>Leia com aten√ß√£o:</strong>
+     Se voc√™ for pagar com o cart√£o de cr√©dito, preencha o seu <span>nome</span> e <span>endere√ßo</span> exatamente como est√° impresso em sua fatura.
+   </p>
+</section> 
 <form id='dbem-rsvp-form' name='booking-form' method='post' action='<?php echo $destination ?>'>
-	<fieldset id="" class="">
-		<p class="grid_4">
+	<fieldset>
+		<p class="wrap">
 			<?php #Nome completo ?>
 			<label for=""><?php _e('Name', 'dbem') ?>:</label>
-			<input type='text' name='person_name' value='<?php echo $_POST['person_name'] ?>'/>
+			<input type='text' id='person_name' name='person_name' value='<?php echo $_POST['person_name'] ?>'/>
 		</p>
-		<p class="grid_4">
+		<p class="wrap">
 			<?php #Email, validado via JS. ?>
 			<label><?php _e('E-Mail', 'dbem') ?>:</label>
-			<input type='text' name='person_email' value='<?php echo $_POST['person_email'] ?>'/>
+			<input type='text' id="person_email" name='person_email' value='<?php echo $_POST['person_email'] ?>'/>
 		</p>
 		
-		<p class="grid_4">
-			<?php /*	Daqui em diante, s„o os campos do endereÁo do cliente.
-						Estes devem ser separados em v·rios outros campos, como mostrado abaixo, ou o PagSeguro
-						recusar· o pagamento imediatamente.
-						Recomendo manifestar de alguma forma que o endereÁo deve ser igual ao do recebimento
-						da fatura de cart„o de crÈdito (se for usado), do contr·rio o PagSeguro pode recusar o
-						pagamento quando for conferir os dados (o que È muito inconveniente para nÛs, pois atÈ
-						isso acontecer, o cliente ficar· registrado como pagante */ ?>
+		<p class="wrap">
+			<?php /*	Daqui em diante, sÔøΩo os campos do endereÔøΩo do cliente.
+						Estes devem ser separados em vÔøΩrios outros campos, como mostrado abaixo, ou o PagSeguro
+						recusarÔøΩ o pagamento imediatamente.
+						Recomendo manifestar de alguma forma que o endereÔøΩo deve ser igual ao do recebimento
+						da fatura de cartÔøΩo de crÔøΩdito (se for usado), do contrÔøΩrio o PagSeguro pode recusar o
+						pagamento quando for conferir os dados (o que ÔøΩ muito inconveniente para nÔøΩs, pois atÔøΩ
+						isso acontecer, o cliente ficarÔøΩ registrado como pagante */ ?>
 			<?php #Rua. SOMENTE A RUA. ?>
-			<label><?php _e('Address', 'dbem') ?>:</label>
-			<input type='text' name='person_address' value='<?php echo $_POST['person_address'] ?>'/>
+			<label for="person_address"><?php _e('Address', 'dbem') ?>:</label>
+			<input type='text' id='person_address' name='person_address' value='<?php echo $_POST['person_address'] ?>'/>
 		</p>
-		<p class="grid_4">
-			<?php #N˙mero do imÛvel. SOMENTE O N⁄MERO. SOMENTE ALGARISMOS NUM…RICOS.  ?>
+		<p class="wrap">
+			<?php #NÔøΩmero do imÔøΩvel. SOMENTE O NÔøΩMERO. SOMENTE ALGARISMOS NUMÔøΩRICOS.  ?>
 			<label for="person_num"><?php _e('Number', 'dbem') ?>:</label>
-			<input type='text' name='person_num' value='<?php echo $_POST['person_num'] ?>'/>	
+			<input type='text' id='person_num' name='person_num' value='<?php echo $_POST['person_num'] ?>'/>	
 		</p>
 		
-		<p class="grid_4">
+		<p class="wrap">
 			<?php #Complemento (apto, bloco, etc). Pode ser vazio. ?>
 			<label for="person_compl"><?php _e('Complement', 'dbem') ?>:</label>
-			<input type='text' name='person_compl' value='<?php echo $_POST['person_compl'] ?>'/>
+			<input type='text' id="person_comp1" name='person_compl' value='<?php echo $_POST['person_compl'] ?>'/>
 		</p>
 		
-		<p class="grid_4">
+		<p class="wrap">
 			<?php #Bairro. ?>
 			<label for="person_district"><?php _e('District', 'dbem') ?>:</label>
-			<input type='text' name='person_district' value='<?php echo $_POST['person_district'] ?>'/>
+			<input type='text' id-"person_district" name='person_district' value='<?php echo $_POST['person_district'] ?>'/>
 		</p>
 		
-		<p class="grid_4">
-			<?php #Cidade, por extenso. N„o abrevie. ?>
+		<p class="wrap">
+			<?php #Cidade, por extenso. NÔøΩo abrevie. ?>
 			<label for="person_city"><?php _e('City', 'dbem') ?>:</label>
-			<input type='text' name='person_city' value='<?php echo $_POST['person_city'] ?>'/>
+			<input type='text' id="person_city" name='person_city' value='<?php echo $_POST['person_city'] ?>'/>
 		</p>
 		
-		<p class="grid_4">
-			<?php #Sigla de duas letras do estado. Irei trocar por um menu de seleÁ„o.
-				# SOMENTE DOIS CARACTERES. SOMENTE UM ESTADO BRASILEIRO V¡LIDO, ou distrito federal (DF) ?>
+		<p class="wrap">
+			<?php #Sigla de duas letras do estado. Irei trocar por um menu de seleÔøΩÔøΩo.
+				# SOMENTE DOIS CARACTERES. SOMENTE UM ESTADO BRASILEIRO VÔøΩLIDO, ou distrito federal (DF) ?>
 			<label for="person_uf"><?php _e('UF', 'dbem') ?>:</label>
-			<input type='text' name='person_uf' value='<?php echo $_POST['person_uf'] ?>' size='2'/>
+			<select id="person_uf" name='person_uf' value='<?php echo $_POST['person_uf'] ?>'>
+        <option value="AC">AC</option>
+        <option value="AL">AL</option>
+        <option value="AM">AM</option>
+        <option value="AP">AP</option>
+        <option value="BA">BA</option>
+        <option value="CE">CE</option>
+        <option value="DF">DF</option>
+        <option value="ES">ES</option>
+        <option value="GO">GO</option>
+        <option value="MA">MA</option>
+        <option value="MG">MG</option>
+        <option value="MS">MS</option>
+        <option value="MT">MT</option>
+        <option value="PA">PA</option>
+        <option value="PB">PB</option>
+        <option value="PE">PE</option>
+        <option value="PI">PI</option>
+        <option value="PR">PR</option>
+        <option value="RJ">RJ</option>
+        <option value="RN">RN</option>
+        <option value="RO">RO</option>
+        <option value="RR">RR</option>
+        <option value="RS">RS</option>
+        <option value="SC">SC</option>
+        <option value="SE">SE</option>
+        <option value="SP">SP</option>
+        <option value="TO">TO</option>
+      </select>
+
 		</p>
 		
-		<p class="grid_4">
-			<?php #Cep. SOMENTE N⁄MEROS. SOMENTE UM CEP V¡LIDO. Recomendo m·scara. Posso remover hÌphens/etc se necess·rio. ?>
+		<p class="wrap">
+			<?php #Cep. SOMENTE NÔøΩMEROS. SOMENTE UM CEP VÔøΩLIDO. Recomendo mÔøΩscara. Posso remover hÔøΩphens/etc se necessÔøΩrio. ?>
 			<label for="person_zip"><?php _e('Zip', 'dbem') ?>:</label>
-			<input type='text' name='person_zip' value='<?php echo $_POST['person_zip'] ?>'/>
+			<input type='text' id='person_zip' name='person_zip' value='<?php echo $_POST['person_zip'] ?>'/>
 		</p>
 		
-		<p class="grid_4">
-			<?php #Dois campos de n˙mero de telefone. Se for adequado, podem ser unidos, mas ent„o sera necess·rio o uso de m·scaras
-				# para deixar claro aonde um acaba e o outro comeÁa (ex. forÁar ddd/telefone separados por um hÌphen) ?>
+		<p class="wrap">
+			<?php #Dois campos de nÔøΩmero de telefone. Se for adequado, podem ser unidos, mas entÔøΩo sera necessÔøΩrio o uso de mÔøΩscaras
+				# para deixar claro aonde um acaba e o outro comeÔøΩa (ex. forÔøΩar ddd/telefone separados por um hÔøΩphen) ?>
 			<label for="person_ddd"><?php _e('Phone number', 'dbem') ?>:</label>
-			<?php #DDD. SOMENTE N⁄MEROS. SOMENTE DOIS CARACTERES. ?>
-			<input type='text' name='person_ddd' value='<?php echo $_POST['person_ddd'] ?>' size='2' />
-			<?php #Telefone. SEM O DDDD. SOMENTE N⁄MEROS. ?>
-			<input type='text' name='person_phone' value='<?php echo $_POST['person_phone'] ?>'/>
+			<?php #DDD. SOMENTE NÔøΩMEROS. SOMENTE DOIS CARACTERES. ?>
+			<input type='text' id='person_ddd' name='person_ddd' value="<?php echo $_POST['person_ddd'] ?>" size="2" />
+			<?php #Telefone. SEM O DDDD. SOMENTE NÔøΩMEROS. ?>
+			<input type='text' id="person_phone" name='person_phone' value='<?php echo $_POST['person_phone'] ?>'/>
 		</p>
 		
 	</fieldset>
 				
 			<p>
-				<input type='submit' value='<?php _e('Send your booking', 'dbem') ?>'/>&nbsp;&nbsp;&nbsp;&nbsp;
+				<input type='submit' class="send" value='<?php _e('Send your booking', 'dbem') ?>'/>&nbsp;&nbsp;&nbsp;&nbsp;
 				<input type='hidden' name="booking_seats" value="1" />
 				<input type='hidden' name='person_country' value='BRA' />
 				<input type='hidden' name='eventAction' value='add_booking'/>
