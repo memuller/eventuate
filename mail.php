da2f060a771d3e3ec2e11f033509c0315140dacf
Matheus E. Muller <hello@memuller.com>
2010-12-23 09:04:08 -0200

    Calls up new mailer method on the mailer.

--- mail.php
+++ mail.php
@@ -1,8 +1,10 @@
 <?php require_once('../../../wp-load.php') ; 
 
-  if($_GET['id'] && is_numeric($_GET['id'])) {
-    $EM_Booking = new EM_Booking($_GET['id']) ;
-    $EM_Booking->payment_reminder_email() ;
-  }
+if($_GET['id'] && is_numeric($_GET['id'])) {
+  $Em_Mailer = new EM_Mailer() ;
+  $EM_Booking = new EM_Booking($_GET['id']) ;
+  $EM_Bookings = new EM_Bookings($EM_Booking->event_id) ;
+  $EM_Bookings->payment_reminder_email($EM_Booking) ;
+}
 
 ?>
